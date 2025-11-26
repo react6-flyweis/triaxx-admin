6c4f5394e4a401ea3c4a23e8d057e81d8878ce4b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-11-26 18:27:19 +0530

    feat: improve sidebar layout in withAdminLayout component

--- src/Views/AdminPanel/withAdminLayout.jsx
+++ src/Views/AdminPanel/withAdminLayout.jsx
@@ -1,18 +1,18 @@
 // src/hoc/withAdminLayout.js
-import React, { useState } from 'react';
-import Sidebar from '../../Components/Sidebar/Sidebar';
-import Topbar from '../../Components/Topbar/Topbar';
+import { useState } from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Topbar from "../../Components/Topbar/Topbar";
 
 const withAdminLayout = (WrappedComponent) => {
   return function AdminLayoutWrapper(props) {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
-      setIsSidebarOpen(prev => !prev);
+      setIsSidebarOpen((prev) => !prev);
     };
 
     return (
-      <div className="flex h-screen overflow-hidden">
+      <div className="flex">
         {/* Sidebar - Desktop */}
         <div className="hidden md:block w-[250px] shrink-0">
           <Sidebar />
@@ -21,7 +21,7 @@ const withAdminLayout = (WrappedComponent) => {
         {/* Sidebar - Mobile Slide-in */}
         <div
           className={`fixed inset-0 z-50 flex md:hidden transition-transform duration-300 ${
-            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           }`}
         >
           {/* Backdrop */}
@@ -37,12 +37,12 @@ const withAdminLayout = (WrappedComponent) => {
         </div>
 
         {/* Main Content */}
-        <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex flex-col overflow-x-hidden min-h-0">
           {/* Topbar */}
           <Topbar onMenuClick={toggleSidebar} />
 
           {/* Page Content */}
-          <div className="p-4 overflow-auto flex-1">
+          <div className="p-4 flex-1 w-full">
             <WrappedComponent {...props} />
           </div>
         </div>
