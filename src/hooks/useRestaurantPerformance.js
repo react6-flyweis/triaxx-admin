7f5b6877d1328e60657be14b8b7baede4fb8e82a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-11-29 23:40:57 +0530

    feat: add useRestaurantPerformance hook to fetch and manage restaurant performance data

---
+++ src/hooks/useRestaurantPerformance.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { reportsService } from "@/services/reportsService";
+
+export function useRestaurantPerformance(restaurantId, options = {}) {
+  return useQuery({
+    queryKey: ["restaurant_performance", restaurantId],
+    queryFn: async () => {
+      const res = await reportsService.getRestaurantPerformance(restaurantId);
+      // reportsService returns the API response (with `data` property)
+      // be resilient: return `res.data` if available, otherwise return res
+      return res?.data ?? res;
+    },
+    enabled: Boolean(restaurantId),
+    staleTime: 1000 * 60, // 1 minute
+    retry: 1,
+    refetchOnWindowFocus: false,
+    ...options,
+  });
+}
